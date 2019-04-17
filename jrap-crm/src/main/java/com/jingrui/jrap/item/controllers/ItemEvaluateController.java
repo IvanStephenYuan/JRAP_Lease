c484b91dbf2a1aceb8a72b75614513df79695b47
IvanStephen <Ivan.Stphen.Y@gmail.com>
2019-04-17 15:00:34 +0800

    删除空行

--- jrap-crm/src/main/java/com/jingrui/jrap/item/controllers/ItemEvaluateController.java
+++ jrap-crm/src/main/java/com/jingrui/jrap/item/controllers/ItemEvaluateController.java
@@ -12,12 +12,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.validation.BindingResult;
+
 import java.util.List;
 
-    @Controller
-    public class ItemEvaluateController extends BaseController{
+@Controller
+public class ItemEvaluateController extends BaseController {
 
     @Autowired
     private IItemEvaluateService service;
@@ -26,19 +29,19 @@ import java.util.List;
     @RequestMapping(value = "/afd/item/evaluate/query")
     @ResponseBody
     public ResponseData query(ItemEvaluate dto, @RequestParam(defaultValue = DEFAULT_PAGE) int page,
-        @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
+                              @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
         IRequest requestContext = createRequestContext(request);
-        return new ResponseData(service.select(requestContext,dto,page,pageSize));
+        return new ResponseData(service.select(requestContext, dto, page, pageSize));
     }
 
     @RequestMapping(value = "/afd/item/evaluate/submit")
     @ResponseBody
-    public ResponseData update(@RequestBody List<ItemEvaluate> dto, BindingResult result, HttpServletRequest request){
+    public ResponseData update(@RequestBody List<ItemEvaluate> dto, BindingResult result, HttpServletRequest request) {
         getValidator().validate(dto, result);
         if (result.hasErrors()) {
-        ResponseData responseData = new ResponseData(false);
-        responseData.setMessage(getErrorMessage(result, request));
-        return responseData;
+            ResponseData responseData = new ResponseData(false);
+            responseData.setMessage(getErrorMessage(result, request));
+            return responseData;
         }
         IRequest requestCtx = createRequestContext(request);
         return new ResponseData(service.batchUpdate(requestCtx, dto));
@@ -46,17 +49,17 @@ import java.util.List;
 
     @RequestMapping(value = "/afd/item/evaluate/remove")
     @ResponseBody
-    public ResponseData delete(HttpServletRequest request,@RequestBody List<ItemEvaluate> dto){
+    public ResponseData delete(HttpServletRequest request, @RequestBody List<ItemEvaluate> dto) {
         service.batchDelete(dto);
         return new ResponseData();
     }
 
-        @RequestMapping(value = "/afd/item/evaluate/selectByItemId/{itemId}")
-        @ResponseBody
-        public ResponseData selectByItemId(@PathVariable String itemId, @RequestParam(defaultValue = DEFAULT_PAGE) int page,
-            @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
-            IRequest requestContext = createRequestContext(request);
-            return new ResponseData(service.selectByItemId(Long.parseLong(itemId),requestContext,page, pageSize));
-        }
+    @RequestMapping(value = "/afd/item/evaluate/selectByItemId/{itemId}")
+    @ResponseBody
+    public ResponseData selectByItemId(@PathVariable String itemId, @RequestParam(defaultValue = DEFAULT_PAGE) int page,
+                                       @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
+        IRequest requestContext = createRequestContext(request);
+        return new ResponseData(service.selectByItemId(Long.parseLong(itemId), requestContext, page, pageSize));
     }
+}
 
