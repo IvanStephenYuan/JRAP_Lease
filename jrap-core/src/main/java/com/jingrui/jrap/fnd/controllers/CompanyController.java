306ccf377c93d82aab382774a93370fa530f2f9b
IvanStephen <Ivan.Stphen.Y@gmail.com>
2019-05-24 16:49:07 +0800

    修改商户接口

--- jrap-core/src/main/java/com/jingrui/jrap/fnd/controllers/CompanyController.java
+++ jrap-core/src/main/java/com/jingrui/jrap/fnd/controllers/CompanyController.java
@@ -2,6 +2,7 @@ package com.jingrui.jrap.fnd.controllers;
 
 import com.jingrui.jrap.core.IRequest;
 import com.jingrui.jrap.fnd.dto.Company;
+import com.jingrui.jrap.fnd.mapper.CompanyMapper;
 import com.jingrui.jrap.fnd.service.ICompanyService;
 import com.jingrui.jrap.fnd.util.GetLatAndLngByBaidu;
 import com.jingrui.jrap.mybatis.common.Criteria;
@@ -29,13 +30,16 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping(value = {"/fnd/company", "/api/fnd/company"})
-@Api(value="ProductController", tags = {"商户维护接口"})
+@Api(value = "ProductController", tags = {"商户维护接口"})
 public class CompanyController extends BaseController {
     @Autowired
     ICompanyService companyService;
 
+    @Autowired
+    CompanyMapper companyMapper;
+
     @RequestMapping(value = "/query", method = {RequestMethod.GET, RequestMethod.POST})
-    @ApiOperation(value="获取商户信息", notes = "通用商户查询接口", httpMethod="GET", response=Company.class)
+    @ApiOperation(value = "获取商户信息", notes = "通用商户查询接口", httpMethod = "GET", response = Company.class)
     public ResponseData select(Company company, HttpServletRequest request, HttpServletResponse response, @RequestParam(defaultValue = DEFAULT_PAGE) int page,
                                @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pagesize) {
         IRequest requestCtx = createRequestContext(request);
@@ -44,8 +48,15 @@ public class CompanyController extends BaseController {
         return new ResponseData(companyService.selectOptions(requestCtx, company, criteria, page, pagesize));
     }
 
+    @RequestMapping(value = "/queryAll", method = {RequestMethod.GET, RequestMethod.POST})
+    @ApiOperation(value = "查询全部商户信息", notes = "查询全部商户信息接口", httpMethod = "POST", response = Company.class)
+    public ResponseData selectAll(Company company, HttpServletRequest request, HttpServletResponse response, @RequestParam(defaultValue = DEFAULT_PAGE) int page,
+                               @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pagesize) {
+        return new ResponseData(companyMapper.select(company));
+    }
+
     @PostMapping(value = {"/submit", "/update"})
-    @ApiOperation(value="商户提交/修改", notes = "商户提交/修改接口(批量)", httpMethod="GET", response=Company.class)
+    @ApiOperation(value = "商户提交/修改", notes = "商户提交/修改接口(批量)", httpMethod = "GET", response = Company.class)
     public ResponseData update(@RequestBody List<Company> companies, BindingResult result, HttpServletRequest request) {
         IRequest requestCtx = createRequestContext(request);
         getValidator().validate(companies, result);
@@ -56,9 +67,9 @@ public class CompanyController extends BaseController {
         }
 
         //批量更新经纬度
-        for(Company company:companies){
+        for (Company company : companies) {
             //如果地址不为空，获取经纬度
-            if(company.getAddress() != null){
+            if (company.getAddress() != null) {
                 Map<String, String> map = GetLatAndLngByBaidu.getLatitude(company.getAddress());
                 company.setAttribute1(map.get("lng"));  //经度
                 company.setAttribute2(map.get("lat"));  //维度
@@ -69,14 +80,14 @@ public class CompanyController extends BaseController {
     }
 
     @PostMapping(value = "/remove")
-    @ApiOperation(value="删除商户信息", notes = "商户删除接口（批量）", httpMethod="GET", response=Company.class)
+    @ApiOperation(value = "删除商户信息", notes = "商户删除接口（批量）", httpMethod = "GET", response = Company.class)
     public ResponseData delete(@RequestBody List<Company> companies, HttpServletRequest request) {
         companyService.batchDelete(companies);
         return new ResponseData();
     }
 
     @PostMapping(value = {"/add"})
-    @ApiOperation(value="添加商户", notes = "添加商户接口", httpMethod="GET", response=Company.class)
+    @ApiOperation(value = "添加商户", notes = "添加商户接口", httpMethod = "GET", response = Company.class)
     public ResponseData update(@RequestBody Company company, BindingResult result, HttpServletRequest request) {
         IRequest requestCtx = createRequestContext(request);
         if (result.hasErrors()) {
@@ -86,13 +97,13 @@ public class CompanyController extends BaseController {
         }
 
         //如果地址不为空，获取经纬度
-        if(company.getAddress() != null){
+        if (company.getAddress() != null) {
             Map<String, String> map = GetLatAndLngByBaidu.getLatitude(company.getAddress());
             company.setAttribute1(map.get("lng"));  //经度
             company.setAttribute2(map.get("lat"));  //维度
         }
 
-        Company company1 = companyService.insertSelective(requestCtx,company);
+        Company company1 = companyService.insertSelective(requestCtx, company);
         List<Company> list = new ArrayList<>();
         list.add(company1);
         return new ResponseData(list);
